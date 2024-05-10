206a98adf1c79414649890043104873c4cf18507
plbalmeida <plbalmeida@usp.br>
2024-05-10 03:01:35 -0300

    fix(*): health checks adicionados

--- src/app.py
+++ src/app.py
@@ -19,5 +19,10 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return "OK", 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)

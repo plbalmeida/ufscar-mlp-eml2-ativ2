fcd40d7ff59d2f306e643effc4e6a52a2e8daaa2
plbalmeida <plbalmeida@usp.br>
2024-05-10 02:38:38 -0300

    fix(infra/ecs/main.tf): correção de portas e parâmetros

--- infra/ecs/main.tf
+++ infra/ecs/main.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "sagemaker_execution_role" {
-  name = "SageMakerExecutionRole"
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -8,61 +8,185 @@ resource "aws_iam_role" "sagemaker_execution_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "sagemaker.amazonaws.com"
+          Service = "ecs-tasks.amazonaws.com"
         }
       },
     ]
   })
 }
 
-resource "aws_iam_policy" "sagemaker_access" {
-  name = "SageMakerAccessPolicy"
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:BatchCheckLayerAvailability",
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Effect   = "Allow",
-        Resource = "*"
-      },
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "sagemaker_policy_attachment" {
-  role       = aws_iam_role.sagemaker_execution_role.name
-  policy_arn = aws_iam_policy.sagemaker_access.arn
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "ecs-cluster"
 }
 
-resource "aws_sagemaker_model" "iris_model" {
-  name               = "iris-model"
-  execution_role_arn = aws_iam_role.sagemaker_execution_role.arn
+resource "aws_ecs_task_definition" "ecs_task" {
+  family                   = "ecs-service"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+  cpu                      = "256"
+  memory                   = "512"
 
-  primary_container {
-    image = var.docker_image
+  container_definitions = jsonencode([
+    {
+      name      = "ufscar-mlp-eml2-ativ2-container"
+      image     = var.docker_image
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "ecs_subnet1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "ecs_subnet2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs_security_group"
+  description = "Security group for ECS in VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_sagemaker_endpoint_configuration" "iris_model_endpoint_config" {
-  name = "iris-model-endpoint-config"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
 
-  production_variants {
-    variant_name           = "AllTraffic"
-    model_name             = aws_sagemaker_model.iris_model.name
-    initial_instance_count = 1
-    instance_type          = "ml.t2.medium"
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
-resource "aws_sagemaker_endpoint" "example" {
-  name                 = "iris-model-endpoint"
-  endpoint_config_name = aws_sagemaker_endpoint_configuration.iris_model_endpoint_config.name
+resource "aws_route_table_association" "a1" {
+  subnet_id      = aws_subnet.ecs_subnet1.id
+  route_table_id = aws_route_table.rtb.id
+}
 
-  depends_on = [aws_sagemaker_model.iris_model]
+resource "aws_route_table_association" "a2" {
+  subnet_id      = aws_subnet.ecs_subnet2.id
+  route_table_id = aws_route_table.rtb.id
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = "ecs-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.ecs_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+    container_name   = "ufscar-mlp-eml2-ativ2-container"
+    container_port   = 8080
+  }
+
+  network_configuration {
+    subnets          = [aws_subnet.ecs_subnet1.id, aws_subnet.ecs_subnet2.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+
+  depends_on = [
+    aws_ecs_task_definition.ecs_task,
+    aws_ecs_cluster.ecs_cluster,
+    aws_lb_listener.ecs_listener
+  ]
+}
+
+resource "aws_lb" "ecs_alb" {
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ecs_sg.id]
+  subnets            = [aws_subnet.ecs_subnet1.id, aws_subnet.ecs_subnet2.id]
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+  name        = "ecs-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    protocol            = "HTTP"
+    path                = "/predict"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_lb_listener" "ecs_listener" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_lb_target_group.ecs_tg]
 }
